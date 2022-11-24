636b47548fdd277b6b56556057ae01a4506c7e32
Rusandor <ruslanpalkin44@gmail.com>
2022-11-24 19:13:17 +0300

    Second task

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import sys
 from random import randint
 
-from PyQt5 import uic
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPainter, QBrush
+from PyQt5.QtGui import QPainter, QBrush, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
+from Ui import Ui_MainWindow
 
-class MyWidget(QMainWindow):
+
+class MyWidget(Ui_MainWindow, QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('Ui.ui', self)
+        self.setupUi(self)
 
         self.setFixedSize(960, 480)
         self.update_count = 0
@@ -23,7 +24,7 @@ class MyWidget(QMainWindow):
             return
 
         painter = QPainter(self)
-        painter.setBrush(QBrush(Qt.yellow, Qt.SolidPattern))
+        painter.setBrush(QBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)), Qt.SolidPattern))
         x, y = randint(25, self.frameGeometry().width() - 25), randint(25, self.frameGeometry().height() - 25)
         diameter = randint(25, min(self.frameGeometry().width() - x, self.frameGeometry().height() - y))
         painter.drawEllipse(x, y, diameter, diameter)
